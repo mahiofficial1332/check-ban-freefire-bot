bad24b2b848e137f7f4048085032a0d3a87f14f0
mahiofficial1332 <sumaiyaislamfojilat2003@gmail.com>
2025-09-15 15:29:11 +0600

    utils.py

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ import asyncio # Import asyncio for timeout
 load_dotenv()
 
 async def check_ban(uid: str) -> dict | None:
-     api_url = f"http://raw.thug4ff.com/check_ban/{uid}"
+    api_url = f"http://raw.thug4ff.com/check_ban/{uid}"
     
-    timeout = aiohttp.ClientTimeout(total=10) # 10 seconds total timeout
+    timeout = aiohttp.ClientTimeout(total=10)  # 10 seconds total timeout
 
     try:
         async with aiohttp.ClientSession(timeout=timeout) as session:
